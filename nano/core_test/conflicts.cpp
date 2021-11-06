07c8237e52450ecea3d4d36f76726f1fcdcd719a
coranos <coranos@users.noreply.github.com>
2021-11-06 18:59:48 -0500

    re-adding test add_two

--- nano/core_test/conflicts.cpp
+++ nano/core_test/conflicts.cpp
@@ -51,25 +51,6 @@ TEST (conflicts, add_existing)
 
 TEST (conflicts, add_two)
 {
-	nano::system system (1);
-	auto & node1 (*system.nodes[0]);
-	nano::keypair key1;
-	auto send1 (std::make_shared<nano::send_block> (nano::dev::genesis->hash (), key1.pub, 0, nano::dev::genesis_key.prv, nano::dev::genesis_key.pub, 0));
-	node1.work_generate_blocking (*send1);
-	ASSERT_EQ (nano::process_result::progress, node1.process (*send1).code);
-	node1.block_confirm (send1);
-	node1.active.election (send1->qualified_root ())->force_confirm ();
-	nano::keypair key2;
-	auto send2 (std::make_shared<nano::send_block> (send1->hash (), key2.pub, 0, nano::dev::genesis_key.prv, nano::dev::genesis_key.pub, 0));
-	node1.work_generate_blocking (*send2);
-	ASSERT_EQ (nano::process_result::progress, node1.process (*send2).code);
-	node1.scheduler.activate (nano::dev::genesis_key.pub, node1.store.tx_begin_read ());
-	node1.scheduler.flush ();
-	ASSERT_EQ (2, node1.active.size ());
-}
-
-TEST (conflicts, add_two)
-{
 	nano::system system{};
 	auto const & node = system.add_node ();
 
