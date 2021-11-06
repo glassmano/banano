c3eb619100ee682f0415e878d4146c84bd07bf05
coranos <coranos@users.noreply.github.com>
2021-11-06 18:57:52 -0500

    re-adding test add_two

--- nano/core_test/conflicts.cpp
+++ nano/core_test/conflicts.cpp
@@ -49,10 +49,7 @@ TEST (conflicts, add_existing)
 	ASSERT_NE (votes.end (), votes.find (key2.pub));
 }
 
-// Test disabled because it's failing intermittently.
-// PR in which it got disabled: https://github.com/nanocurrency/nano-node/pull/3536
-// Issue for investigating it: https://github.com/nanocurrency/nano-node/issues/3535
-TEST (conflicts, DISABLED_add_two)
+TEST (conflicts, add_two)
 {
 	nano::system system (1);
 	auto & node1 (*system.nodes[0]);
