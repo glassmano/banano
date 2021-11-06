d90742c63b473587fe918299a0682446a494cff6
coranos <coranos@users.noreply.github.com>
2021-11-06 17:32:44 -0500

    making shell files same

--- ci/actions/linux/install_deps.sh
+++ ci/actions/linux/install_deps.sh
@@ -4,8 +4,8 @@ sudo mkdir -p /etc/docker && echo '{"ipv6":true,"fixed-cidr-v6":"2001:db8:1::/64
 
 ci/build-docker-image.sh docker/ci/Dockerfile-base bananocoin/nano-env:base
 if [[ "${COMPILER:-}" != "" ]]; then
-   ci/build-docker-image.sh docker/ci/Dockerfile-gcc bananocoin/nano-env:${COMPILER}
+    ci/build-docker-image.sh docker/ci/Dockerfile-gcc bananocoin/nano-env:${COMPILER}
 else
-   ci/build-docker-image.sh docker/ci/Dockerfile-gcc nanocurrency/nano-env:gcc
-   ci/build-docker-image.sh docker/ci/Dockerfile-clang-6 nanocurrency/nano-env:clang-6
+    ci/build-docker-image.sh docker/ci/Dockerfile-gcc nanocurrency/nano-env:gcc
+    ci/build-docker-image.sh docker/ci/Dockerfile-clang-6 nanocurrency/nano-env:clang-6
 fi
