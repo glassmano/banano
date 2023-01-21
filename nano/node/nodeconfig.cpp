ad46e638dc4cc5c23a1614fd133f0cbc9b3de7c6
coranos <coranos@users.noreply.github.com>
2023-01-20 22:01:24 -0600

    typo

--- nano/node/nodeconfig.cpp
+++ nano/node/nodeconfig.cpp
@@ -15,7 +15,7 @@ namespace
 char const * preconfigured_peers_key = "preconfigured_peers";
 char const * signature_checker_threads_key = "signature_checker_threads";
 char const * pow_sleep_interval_key = "pow_sleep_interval";
-std::string const default_live_peer_network = nano::get_env_or_default ("NANO_DEFAULT_PEER", "livenet.banano.cc);
+std::string const default_live_peer_network = nano::get_env_or_default ("NANO_DEFAULT_PEER", "livenet.banano.cc");
 std::string const default_beta_peer_network = nano::get_env_or_default ("NANO_DEFAULT_PEER", "livenet-beta.banano.cc");
 std::string const default_test_peer_network = nano::get_env_or_default ("NANO_DEFAULT_PEER", "peering-test.nano.org");
 }
