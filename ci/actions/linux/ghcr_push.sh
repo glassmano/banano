f60f75729fcf5c112464d8a6ff8e6b947310483b
coranos <coranos@users.noreply.github.com>
2023-01-21 15:17:20 -0600

    fixing typos

--- ci/actions/linux/ghcr_push.sh
+++ ci/actions/linux/ghcr_push.sh
@@ -2,7 +2,7 @@
 set -e
 
 scripts="$PWD/ci"
-if [[ "$GITHUB_WORKFLOW" = "Develop" ]]; then
+if [[ "$GITHUB_WORKFLOW" = "Develop Branch Dockers Deploy" ]]; then
     "$scripts"/custom-timeout.sh 30 docker push "ghcr.io/bananocoin/nano-env:base"
     "$scripts"/custom-timeout.sh 30 docker push "ghcr.io/bananocoin/nano-env:gcc"
     "$scripts"/custom-timeout.sh 30 docker push "ghcr.io/bananocoin/nano-env:clang-6"
