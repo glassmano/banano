ca6b72061b2909d54d548138190da72a7186bcd7
coranos <coranos@users.noreply.github.com>
2023-01-22 15:22:45 -0600

    updating v24

--- nano/node/nodeconfig.hpp
+++ nano/node/nodeconfig.hpp
@@ -53,7 +53,7 @@ public:
 	nano::amount rep_crawler_weight_minimum{ "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF" };
 	std::chrono::milliseconds vote_generator_delay{ std::chrono::milliseconds (100) };
 	unsigned vote_generator_threshold{ 3 };
-	nano::amount online_weight_minimum{ 60000 * nano::MBAN_ratio };
+	nano::amount online_weight_minimum{ 900 * nano::MBAN_ratio };
 	unsigned election_hint_weight_percent{ 50 };
 	unsigned password_fanout{ 1024 };
 	unsigned io_threads{ std::max (4u, nano::hardware_concurrency ()) };
