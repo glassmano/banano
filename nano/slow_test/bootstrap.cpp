3fb0179ddb7693d948c95004203c689bab80d07a
? <?@users.noreply.github.com>
2024-09-13 18:30:49 -0500

    fixing typos

--- nano/slow_test/bootstrap.cpp
+++ nano/slow_test/bootstrap.cpp
@@ -64,7 +64,7 @@ TEST (bootstrap_ascending, profile)
 {
 	nano::test::system system;
 	nano::thread_runner runner{ system.io_ctx, system.logger, 2 };
-	nano::networks network = nano::networks::nano_beta_network;
+	nano::networks network = nano::networks::banano_beta_network;
 	nano::network_params network_params{ network };
 
 	// Set up client and server nodes
